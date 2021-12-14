c598c2dc78a5c1fcd3f823855e67ccf7f5c8fb4d
AnnaEf24 <44416169+AnnaEf24@users.noreply.github.com>
2021-12-14 20:10:10 +0300

    New Sum

--- Anna-Efimova/task_4_SumOfProduction/SumOfProduct.cpp
+++ Anna-Efimova/task_4_SumOfProduction/SumOfProduct.cpp
@@ -1,20 +1,70 @@
-﻿// SumOfProduct.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+long long result;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int maximum(int a, int b) {
+	if (a > b)
+		return a;
+	else
+		return b;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void get_sum(int index, long long arr_prod, bool* if_been_used, int argc, int** arr, int* arr_siz) {
+	if (argc == index) {
+		result += arr_prod;
+		return;
+	}
+	for (int i = 0; i < arr_siz[index]; ++i) {
+		if (if_been_used[i]==0) {
+			if_been_used[i] = true;
+			get_sum(index + 1, arr_prod * arr[index][i], if_been_used, argc, arr, arr_siz);
+			if_been_used[i] = false;
+		}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	}
+}
+
+int str2int(const char* string) {
+	int answer = 0;
+	for (int p = 0; string[p]; ++p) {
+		answer *= 10;
+		answer += string[p] - '0';
+	}
+	return answer;
+}
+
+
+int main(int argc, char** argv) {
+	argc--;
+	int** arr = new int* [argc];
+	int* arr_siz = new int[argc];
+	int size_max = 0;
+
+	for (int i = 0; i < argc; ++i) {
+		arr_siz[i] = str2int(argv[i + 1]);
+		arr[i] = new int[arr_siz[i]];
+		size_max = maximum(arr_siz[i], size_max);
+	}
+
+	bool* if_been_used = new bool[size_max];
+	for (int i = 0; i < size_max; ++i) {
+		if_been_used[i] = false;
+	}
+
+	for (int p = 0; p < argc; ++p) {
+		for (int i = 0; i < arr_siz[p]; ++i) {
+			std::cin >> arr[p][i];
+		}
+	}
+
+	result = 0;
+	get_sum(0, 1, if_been_used, argc, arr, arr_siz);
+	std::cout << result;
+	delete[] arr_siz;
+	delete[] if_been_used;
+	for (int i = 0; i < argc; ++i) {
+		delete[] arr[i];
+	}
+	delete[] arr;
+	return 0;
+}
